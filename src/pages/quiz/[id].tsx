5589ccd1d037e9fb4f818d6dcecfa0782523dcf7
gabriel23002 <gabriel23002@gmail.com>
2023-02-07 00:47:52 -0300

    feat: Create Quiz Anshwers

--- src/pages/quiz/[id].tsx
+++ src/pages/quiz/[id].tsx
@@ -1,80 +1,121 @@
 import { GetServerSideProps } from 'next'
 import Image from 'next/image'
+import { useState } from 'react'
 
 import styles from './Quiz.module.scss'
 
-type Cover = {
-  data: {
+type QuizPageProps = {
+  quizInfo: {
+    id: number;
     attributes: {
-      alternativeText: string;
-      url: string
-    },
-    id: number
-  }
-}
-
-type Question = {
-  attributes: {
-    description: string
-    cover: {
-      data: {
-        attributes: {
-          alternativeText: string
-          url: string
+      title: string;
+      description: string;
+      littleKnowledge: string;
+      mediumKnowledge: string;
+      fullKnowledge: string;
+      cover: {
+        data: {
+          id: number;
+          attributes: {
+            alternativeText: string;
+            url: string;
+          }
         }
+      },
+      questions: {
+        data: [
+          {
+            id: number;
+            attributes: {
+              description: string;
+              cover: {
+                data: {
+                  id: number;
+                  attributes: {
+                    alternativeText: string;
+                    url: string
+                  }
+                }
+              },
+              answers: {
+                data: [
+                  {
+                    id: number;
+                    attributes: {
+                      description: string;
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        ]
       }
     }
-  },
-  id: number
-}
-
-type Tag = {
-  id: number
-  attributes: {
-    description: string
   }
-}
-
-type QuizInfo = {
-  id: number
-  attributes: {
-    cover: Cover
-    description: string
-    title: string
-    fullKnowledge: string
-    mediumKnowledge: string
-    littleKnowledge: string
-    questions: Question[]
-    tags: Tag[]
-  }
-}
-
-type QuizPageProps = {
-  quizInfo: QuizInfo
 }
 
 export default function QuizPage({ quizInfo }: QuizPageProps) {
-  console.log(quizInfo)
+  console.log(quizInfo.attributes.questions.data[0].attributes)
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answhers, setAnswhers] = useState([]);
+  const [option, setOption] = useState(0)
+
+  function InitializeQuiz() {
+    setQuizStarted(true);
+    setCurrentQuestion(quizInfo.attributes.questions.data[0].id)
+  }
+
+
+  const onOptionChange = (id: number) => {
+    setOption(id)
+  }
+ 
   return (
     <div>
       <h1>{quizInfo.attributes.title}</h1>
-
-      <div>
-      <Image
-          className={styles.image} 
-          layout='fill'
-          objectFit='cover'
-          alt={quizInfo.attributes.cover.data.attributes.alternativeText} 
-          src={quizInfo.attributes.cover.data.attributes.url}
-        />
-      </div>
+      {!quizStarted ? (
+        <div>
+          <div>
+            <Image
+              className={styles.image}
+              layout='fill'
+              objectFit='cover'
+              alt={quizInfo.attributes.cover.data.attributes.alternativeText}
+              src={quizInfo.attributes.cover.data.attributes.url}
+            />
+          </div>
+          <h2>{quizInfo.attributes.description}</h2>
+          <button type='button' onClick={InitializeQuiz}>Iniciar Quiz</button>
+        </div>
+      ) : (
+        quizInfo.attributes.questions.data.map(question => (
+          <div key={question.id} className={question.id === currentQuestion ? styles.activeQuestion : styles.disableQuestion}>
+            <Image
+              className={`${styles.image} `}
+              layout='fill'
+              objectFit='cover'
+              alt={question.attributes.cover.data.attributes.alternativeText}
+              src={question.attributes.cover.data.attributes.url}
+            />
+            <h2>{question.attributes.description}</h2>
+            {question.attributes.answers.data.map(answher => (
+              <div key={answher.id} className={styles.answher} onClick={() => onOptionChange(answher.id)}>
+                <input type="radio" value={answher.id} name='anshwer' id={String(answher.id)} checked={answher.id === option} />
+                <label htmlFor="anshwer">{answher.attributes.description}</label>
+              </div>
+            ))}
+          </div>
+        ))
+      )}
 
     </div>
   )
 }
 
 const getQuiz = async (quizId: string) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/quizzes/${quizId}?populate[0]=cover&populate[1]=questions&populate[2]=questions.cover`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/quizzes/${quizId}?populate[0]=cover&populate[1]=questions&populate[2]=questions.cover&populate[3]=questions.answers`)
   const data = await response.json()
   return data.data
 }
