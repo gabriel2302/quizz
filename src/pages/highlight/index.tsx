ca5dd6cb6920cd9a973c12e57ad0b7453a25bb54
gabriel23002 <gabriel23002@gmail.com>
2023-02-26 00:45:48 -0300

    feat: Add load in highlight page

--- src/pages/highlight/index.tsx
+++ src/pages/highlight/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { getPlaiceholder } from 'plaiceholder';
-import { useEffect, useState } from 'react';
+import { PacmanLoader } from 'react-spinners';
+import { CSSProperties, useEffect, useState } from 'react';
 import { FiAward } from 'react-icons/fi';
 import styles from './highlight.module.scss'
 
@@ -15,17 +15,25 @@ type Highlight = {
   base64: string;
 }
 
+const override: CSSProperties = {
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%, -50%)',
+  position: 'absolute'
+};
+
 export default function Highlight() {
   const [highlight, setHighlight] = useState<Highlight[]>([])
+  const [isLoading, setIsLoading] = useState(true)
   const { push } = useRouter();
 
   useEffect(() => {
     const fetchData = async () => {
       const highlightList = await getHighlight();
       setHighlight(highlightList);
+      setIsLoading(false);
     }
     fetchData()
-
   }, []);
 
   const choiceMeanColor = (percentage: number) => {
@@ -40,46 +48,57 @@ export default function Highlight() {
   return (
     <div className={styles.highlightBox}>
       <h1 className={styles.title}>Quiz em alta (Top 10)</h1>
-
-      {highlight.map((item, index) => (
-        <div key={item.quiz_id} className={styles.highlightItem}>
-          <div className={styles.titleBox}>
-            <h2 className={styles.highlightItemTitle}>
-              {item.title}
-            </h2>
-            {index < 3 && (
-              <div className={styles.badge}>
-              <FiAward size={24} className={styles.badgeIcon}/>
-              <span className={styles.badgeCount}>{index + 1}</span>
+      {isLoading ? (
+        <div className="sweet-loading">
+        <PacmanLoader
+          color='#88aaee'
+          loading={isLoading}
+          cssOverride={override}
+          size={32}
+          aria-label="Carregando lista de Quiz"
+          data-testid="loader"
+        />
+      </div>
+      ): 
+        highlight.map((item, index) => (
+          <div key={item.quiz_id} className={styles.highlightItem}>
+            <div className={styles.titleBox}>
+              <h2 className={styles.highlightItemTitle}>
+                {item.title}
+              </h2>
+              {index < 3 && (
+                <div className={styles.badge}>
+                <FiAward size={24} className={styles.badgeIcon}/>
+                <span className={styles.badgeCount}>{index + 1}</span>
+              </div>
+              )}
+              
             </div>
-            )}
-            
+            <div className={styles.highlightItemDescription}>
+              <span>Respondido <span className={styles.amount}>{item.anshwered}</span> {Number(item.anshwered) > 1 ? 'vezes' : 'vez'}</span>
+              <span className={`${styles.mean}`}> - Média de acertos: 
+                <span className={`${styles[choiceMeanColor(item.percentage)]}`}> {item.percentage}%</span>
+              </span>
+            </div>
+            <div style={{ position: 'relative' }}>
+              <Image
+                className={styles.image}
+                fill
+                priority
+                placeholder="blur"
+                blurDataURL={item.base64}
+                style={{ objectFit: 'cover' }}
+                alt={item.alternative_text}
+                src={item.url}
+                sizes="(min-width: 60em) 24vw,
+                      (min-width: 28em) 45vw,
+                      100vw"
+              />
+            </div>
+  
+            <button type='button' className={styles.button} role="link" onClick={() => push(`/quiz/${item.quiz_id}`)}>Fazer quiz</button>
           </div>
-          <div className={styles.highlightItemDescription}>
-            <span>Respondido <span className={styles.amount}>{item.anshwered}</span> {Number(item.anshwered) > 1 ? 'vezes' : 'vez'}</span>
-            <span className={`${styles.mean}`}> - Média de acertos: 
-              <span className={`${styles[choiceMeanColor(item.percentage)]}`}> {item.percentage}%</span>
-            </span>
-          </div>
-          <div style={{ position: 'relative' }}>
-            <Image
-              className={styles.image}
-              fill
-              priority
-              placeholder="blur"
-              blurDataURL={item.base64}
-              style={{ objectFit: 'cover' }}
-              alt={item.alternative_text}
-              src={item.url}
-              sizes="(min-width: 60em) 24vw,
-                    (min-width: 28em) 45vw,
-                    100vw"
-            />
-          </div>
-
-          <button type='button' className={styles.button} role="link" onClick={() => push(`/quiz/${item.quiz_id}`)}>Fazer quiz</button>
-        </div>
-      ))}
+        ))}
     </div>
   )
 }
